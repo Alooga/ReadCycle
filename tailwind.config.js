e4cdb5b2f5a82742645ed6003d6de284bda3e780
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2024-07-26 12:56:33 +0200

    estilos globales

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,29 @@ module.exports = {
           purple: "#9E2DD3",
         },
       },
+      colors:{
+        primary: "#14183E",
+        secondary: '165058',
+      },
+      action: {
+        default: '#165058', 
+        hover: '#165058',   
+        active: '#165058',  
+      },
+      background: {
+        light: '#E3F6F8', 
+      },
+      text: {
+        primary: '#14183E', 
+        secondary: '#1B626D', 
+        muted: '#718096', 
+      },
+      additional: {
+        red: '#CD240D',    
+        yellow: '#EDC22E', 
+        purple: '#9E2DD3',   
+       
+      },
     },
   },
   plugins: [],
