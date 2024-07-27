7dbd77e68c6861fa7bc9b7923df44d417966b453
Nadialagosaez <168412407+Nadialagosaez@users.noreply.github.com>
2024-07-27 13:00:09 +0200

    Revert "Users bd"

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
   <div>
-   
-    <RouterView />
+    <h1>Esto es la home</h1>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
-<script>
-import Home from './views/Home.vue'
-export default {
-  name: "App",
-};
-</script>
-
-<style>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
-/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
