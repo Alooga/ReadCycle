dd725315497024a86a503e3b26adbae4d31b9f78
Alooga <alopezgarr@gmail.com>
2024-07-31 11:55:46 +0200

    dando estructura interna a la book view. modificado route.js para conectar

--- src/route.js
+++ src/route.js
@@ -1,10 +1,12 @@
 import Home from "./views/Home.vue";
 import OfferBooks from "./views/OfferBooks.vue";
 import { createWebHistory, createRouter } from 'vue-router'
+import BookView from './views/BookView.vue'
 
 const routes = [
     { path: "/", name: "home", component: Home },
     { path: "/OfrecerLibro", name: "offerBooks", component: OfferBooks },
+    { path: "/book/:id", name: 'BookView', component: BookView}
 ]
 
 const router = createRouter({
