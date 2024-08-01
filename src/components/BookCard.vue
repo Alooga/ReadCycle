03e4988522a9a5b331b73008f4d752e2affbe52f
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2024-08-01 17:28:44 +0200

    home lista responsive, arreglo botón cards con hover, slider y header. falta responsive del header

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,19 +1,23 @@
 <template>
-<div>
-    <div v-if="book" class="flex gap-8 text-left md:flex-row md:gap-8 bg-white drop-shadow-xl rounded-xl p-5">
+  <div>
+    <div
+      v-if="book"
+      class="flex gap-8 text-left md:flex-row md:gap-8 bg-white drop-shadow-xl rounded-xl p-5"
+    >
       <img
         class="max-w-[120px] h-auto object-contain"
-        :src="book.image" alt="Book Cover"
+        :src="book.image"
+        alt="Book Cover"
       />
       <div class="flex flex-col justify-around gap-3">
         <div class="space-y-3">
           <h3 class="font-serif text-[1.2rem] font-bold">{{ book.title }}</h3>
-        <div v-for="author in book.author">
-          <p class="text-[0.8rem]">{{ author }}</p>
-        </div>
+          <div v-for="author in book.author">
+            <p class="text-[0.8rem]">{{ author }}</p>
+          </div>
         </div>
         <button
-          class="bg-[#207581] text-white border border-[#207581] py-2 px-4 rounded hover:bg-[#155a65] "
+          class= "text-primary border-2 border-[#207581] py-2 px-4 rounded-full hover:bg-[#207581] hover:text-[white]"
         >
           Ver libro
         </button>
@@ -24,7 +28,7 @@
               alt="User icon"
               class="w-4 h-4"
             />
-            <p> {{ book.userName }}</p>
+            <p>{{ book.userName }}</p>
           </div>
           <div>
             <div class="flex items-center gap-1">
@@ -41,19 +45,14 @@
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import { ref, onMounted, computed } from 'vue';
-
+import { ref, onMounted, computed } from "vue";
 
 export default {
   props: {
-    book: Object
-  },  
-
-
-
+    book: Object,
+  },
 };
 </script>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,24 +1,53 @@
 <template>
-    <div class="flex justify-between self-center w-full h-[656px] bg-[#E3F6F8] bg-no-repeat">
-        <img src="../images/BooksLeft.svg" alt="people reading left side">
-        <div class="flex flex-col font-serif font-semibold place-content-center gap-5">            
-            <h1 class="self-center text-7xl"><span class="text-[#1B626D]">Lee,</span> comparte, <span class="text-[#1B626D]">recicla</span></h1>
-            <p class="font-sans text-xl">Dale una nueva vida a tus libros</p>
-            <div class="flex justify-center gap-10 text-lg font-sans py-5">
-                <button class="bg-[#207581] h-12 px-5 text-white rounded-full text-lg">Encuentra mi libro</button>
-                <RouterLink class="flex items-center h-12 border border-[#207581] bg-white px-5 text-[#207581] rounded-full text-lg" to="/offer-book"> Ofrecer Libro </RouterLink>
-                
-            </div>
+  <div class="relative w-full h-auto md:h-[656px] bg-[#E3F6F8] overflow-hidden">
+    <img
+      src="../images/BooksLeft.svg"
+      alt="people reading left side"
+      class="absolute top-0 -left-10 md:left-0 w-1/3 md:w-1/4 h-auto"
+    />
+    <img
+      src="../images/BooksRight.svg"
+      alt="people reading right side"
+      class="absolute top-0 -right-10 md:right-0 w-1/3 md:w-1/4 h-auto"
+    />
+    <div
+      class="relative z-10 flex flex-col justify-center items-center text-center w-full h-full p-8 md:p-8"
+    >
+      <div class="flex flex-col p-8 font-serif w-[80%] gap-5 max-w-lg">
+        <h1
+          class="text-[2.4rem] leading-[3rem] font-bold md:text-5xl md:p-8 lg:text-6xl lg:p-5"
+        >
+          <span class="text-[#1B626D]">Lee,</span> comparte,
+          <span class="text-[#1B626D]">recicla</span>
+        </h1>
+        <p class="font-sans font-regular sm:text-lg lg:text-xl">
+          Dale una nueva vida a tus libros
+        </p>
+        <div
+          class="flex md:flex-row justify-center items-center gap-2 md:gap-4 text-base md:text-lg font-sans py-5 w-full max-w-lg md:max-w-xl mx-auto"
+        >
+          <button
+            class="bg-[#207581] py-3 px-5 text-white rounded-full text-base md:text-lg w-auto md:w-auto"
+          >
+            Buscar libro
+          </button>
+          <RouterLink
+            class="text-primary border-2 bg-white border-[#207581] py-3 px-5 rounded-full hover:bg-[#207581] hover:text-white text-base md:text-lg w-auto md:w-auto"
+            to="/offer-book"
+          >
+            Ofrecer Libro
+          </RouterLink>
         </div>
-        <img src="../images/BooksRight.svg" alt="people reading right side">
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Slider",
-        data () {
-            return
-        }
-    }
+export default {
+  name: "Slider",
+  data() {
+    return;
+  },
+};
 </script>
