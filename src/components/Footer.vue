958273e03d3ae478e56988f70e3f5b9ff2120dc5
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2024-08-06 13:27:28 +0200

    que se abra en otra pagina el link de linkedin

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-[#333333] text-white p-8">
             <ul class="flex justify-around  p-3">
-                <li><a href="https://www.linkedin.com/in/alexandra-lopez-garrido">👉 Alexadra Lopez</a></li>
-                <li><a href="https://www.linkedin.com/in/nadia-lago-saez/">👉 Nadia Lago Saéz</a></li>
-                <li><a href="https://www.linkedin.com/in/jeaneth-sanchez/">👉 Jeaneth Sánchez</a></li>
+                <li><a href="https://www.linkedin.com/in/alexandra-lopez-garrido" target="_blank">👉 Alexadra Lopez</a></li>
+                <li><a href="https://www.linkedin.com/in/nadia-lago-saez/" target="_blank">👉 Nadia Lago Saéz</a></li>
+                <li><a href="https://www.linkedin.com/in/jeaneth-sanchez/" target="_blank">👉 Jeaneth Sánchez</a></li>
             </ul>
         
     </div>
