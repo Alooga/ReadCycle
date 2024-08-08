3774aa1042323f9e56238c2ad63be36f9b6f5527
Nadialagosaez <nadia.lagosaez@gmail.com>
2024-08-08 18:32:42 +0200

    Metodo sin utilizar en apiBookStore

--- src/store/apiBooksStore.js
+++ src/store/apiBooksStore.js
@@ -3,20 +3,9 @@ import { defineStore } from "pinia";
 export const useApiStore = defineStore ('apiBooksStore', { 
     state: () => ({
         books: [],
-        book: null,
+       
     }),
     actions: {
-    //Lamada por Id
-     getBooksByIsbn(isbn) {
-            fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
-            .then((response) => response.json())
-            .then((data) => {
-            this.books = data.items
-        })
-        .catch(error => {
-            console.error("Error fetching books by ISBN:", error);
-        });
-  },
   //llama por keyword
     getBooksByKeyWord(keyWord) {
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyWord}`)
